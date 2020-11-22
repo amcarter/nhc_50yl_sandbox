0a9f1f7a94966ff5add7a50c6766dca861fce2ba
Alice Carter <alicecarter05@gmail.com>
2020-11-22 18:04:01 -0500

    built a fixed K-Q relationship based on Hall data to model metab

--- src/analysis/calculate_k_from_hall_eq.R
+++ src/analysis/calculate_k_from_hall_eq.R
@@ -16,19 +16,27 @@ setwd("C:/Users/Alice Carter/Dropbox (Duke Bio_Ea)/projects/hall_50yl/code")
 
 #fix this version so that it is the one that has discharge
 # you can find how to do that in analyze_hall_k_values
-#hall_k <- read_csv("../data/hall/hall_tableA2_k_morphology_extended.csv") 
-hall_k <- read_csv("../data/hall/hall_tableA2_k_morphology.csv") %>% 
-filter(method == "empirical")
+##Convert KO2 to K600
+K600fromO2<-function(temp, KO2) {
+  ((600/(1800.6-(120.1*temp)+(3.7818*temp^2)-(0.047608*temp^3)))^-0.5)*KO2
+}
+
+hall_k <- read_csv("../data/hall/hall_tableA2_k_morphology_extended.csv")# %>%
+hall_k <- read_csv("../data/hall/hall_k_compiled.csv") %>%
+  filter(method == "empirical") %>%
+  mutate(K600 = K600fromO2(20, KO2.perday))
 
 # d = depth (m), v = velocity (m/s), DO.sat = DO at saturation (mg/L), t = temp C
 setwd("C:/Users/Alice Carter/Dropbox (Duke Bio_Ea)/projects/NHC_2019_metabolism/data")
-cbp <- read_csv("metabolism/processed/CBP.csv") %>%
-  select(-SpecCond_uScm, -discharge, -WaterTemp_C)
+cbp <- read_csv("metabolism/processed/CBP_lvl.csv") 
 
 # this should work for calculating things... but it doesnt for some reason
-cbp$v_ms <- calc_velocity(cbp$discharge_m3s)
-cbp$k2 <- 5.026 * (cbp$v_ms*3.28084)^0.969 * ((cbp$level_m-0.3) * 3.28084)^(-1.673)
-cbp$kO2 <- (2.3 * cbp$DO.sat * cbp$k2 )/(cbp$level_m - 0.46)
+# cbp$v_ms <- calc_velocity(cbp$discharge)
+# cbp$k2 <- 5.026 * (cbp$v_ms*3.28084)^0.969 * ((cbp$depth) * 3.28084)^(-1.673)
+# cbp$kO2 <- (2.3 * cbp$DO.sat * cbp$k2 )/(cbp$depth)
+# 
+# plot(cbp$depth, cbp$k2)#, ylim = c(0,100))
+# points(hall_k$depth.m, hall_k$K2, col = 2)
 
 #hall_k$v_ms <- calc_velocity(hall_k$discharge_m3s)
 # hall_k$calc_v <- 
@@ -36,22 +44,29 @@ cbp$kO2 <- (2.3 * cbp$DO.sat * cbp$k2 )/(cbp$level_m - 0.46)
 # hall_k$kO2 <- (2.3 * 9 * hall_k$k2 * 1.0241^(20 - 20))/(hall_k$depth.m)
 
 # Hall used the numbers from stream morphology for his calculations, so I will too
-hallQ <- range(hall_k$discharge_m3s)
-Q <- range(cbp$discharge_m3s, na.rm = T)
+hallQ <- log(range(hall_k$discharge_m3s))
+# #get the range of all Q's
+# q <- read_csv("metabolism/processed/NHC.csv") %>%
+#   select(discharge)
+# Q <- log(range(q, na.rm = T))
+
+Q <- seq(-9, 5)
+
 comb <- hall_k %>% 
-  select(hall_depth = depth.m, hall_kO2 = KO2.perday, discharge_m3s) %>%
-  full_join(cbp) %>%
-  mutate(logQ = log(discharge_m3s))
+  mutate(logQ = log(discharge_m3s)) %>%
+  select(K600, logQ) %>%
+  full_join(data.frame(nodes = Q,
+                       logQ = Q)) 
 
-comb$hall_kO2[which(comb$discharge_m3s <= hallQ[1])] <- hall_k$KO2.perday[1]
-comb$hall_kO2[which(comb$discharge_m3s >= hallQ[2])] <- 
-  hall_k$KO2.perday[nrow(hall_k)]
+comb$K600[which(comb$logQ <= hallQ[1])] <- hall_k$K600[1]
+comb$K600[which(comb$logQ >= hallQ[2])] <- 
+  hall_k$K600[nrow(hall_k)]
 
-comb_k <- transform(comb, hall_kO2 = na.approx(hall_kO2, logQ)) %>%
-  mutate(K600 = K600fromO2(temp.water, hall_kO2))
+comb_k <- transform(comb, K600 = na.approx(K600, logQ, na.rm = F)) 
+ 
 plot(comb_k$logQ, comb_k$K600)
-ggplot(comb_k, aes(hall_kO2, K600))+
-  geom_point(aes(col = temp.water))
+points(comb_k$nodes, comb_k$K600, col = "red", pch = 19)
+write_csv(comb_k, "siteData/KQ_hall_prior.csv")
 
 # 11/19/2020
 # didn't finish here the plan was to use the hall data that already was 
